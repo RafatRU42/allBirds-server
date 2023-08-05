6188e64b1e1b91a649ee826188dddd5464ce7c69
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-08-05 08:35:56 +0600

    cart data loaded

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -32,6 +32,7 @@ async function run() {
     try{
 
     const allProducts = client.db('allBirds').collection('allProducts')
+    const cartProducts = client.db('allBirds').collection('cart')
 
     app.get('/allProducts',async (req,res) =>{
       const query = {}
@@ -62,6 +63,26 @@ async function run() {
     app.get('/electronics',async(req,res) =>{
       const query = {category:'electronics'}
       const result = await allProducts.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/details/:id',async(req,res) =>{
+      const id = req.params.id;
+      const filter= {_id:new ObjectId(id)}
+      const data = await allProducts.findOne(filter)
+      res.send(data)
+    })
+
+    app.post('/addToCart',async(req,res) =>{
+      const data = req.body;
+      const result = await cartProducts.insertOne(data)
+      res.send(result)
+    })
+
+    app.get('/getCartProduct',async(req,res) =>{
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartProducts.find(query).toArray()
       res.send(result)
     })
 
